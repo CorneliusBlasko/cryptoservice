e44c82796c114f16b330ac1d88345906d2495e03
Alberto <albertomutcirilo@gmail.com>
2020-05-02 20:27:12 +0200

    Refactoring

--- src/main/java/com/crypto/controllers/CryptoPriceController.java
+++ src/main/java/com/crypto/controllers/CryptoPriceController.java
@@ -2,11 +2,8 @@ package com.crypto.controllers;
 
 import com.crypto.model.CryptoRequestData;
 
-import java.io.IOException;
-
 public interface CryptoPriceController{
 
     String getCryptoPrices(CryptoRequestData requestData);
-    String getCryptoPricesTest() throws IOException;
 
 }
--- src/main/java/com/crypto/controllers/CryptoPriceControllerImpl.java
+++ src/main/java/com/crypto/controllers/CryptoPriceControllerImpl.java
@@ -2,11 +2,6 @@ package com.crypto.controllers;
 
 import com.crypto.model.CryptoRequestData;
 import com.crypto.services.CryptoPriceServiceImpl;
-import org.apache.logging.log4j.core.util.IOUtils;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class CryptoPriceControllerImpl implements CryptoPriceController{
 
@@ -20,14 +15,6 @@ public class CryptoPriceControllerImpl implements CryptoPriceController{
         String response;
         response = service.doConnect(requestData.getStart(), requestData.getLimit(), requestData.getConvert());
         return response;
-    }
-
-    public String getCryptoPricesTest() throws IOException{
-        //        File initialFile = new File("response.txt");
-        //        InputStream is = new FileInputStream(initialFile);
-        InputStream is = getClass().getClassLoader().getResourceAsStream("response.txt");
-        InputStreamReader isReader = new InputStreamReader(is);
-        return IOUtils.toString(isReader);
     }
 
 }
