bbf1cfc548a70bd48b7bba81244c02829b21113c
Alberto <albertomutcirilo@gmail.com>
2020-05-10 19:19:37 +0200

    Adding locale formatter

--- src/main/java/com/crypto/model/CryptoQuote.java
+++ src/main/java/com/crypto/model/CryptoQuote.java
@@ -17,8 +17,8 @@ public class CryptoQuote{
     @BsonProperty("name") private String name;
     @BsonProperty("symbol") private String symbol;
     @BsonProperty("currency") private String currency;
-    @BsonProperty("price") private double price;
-    @BsonProperty("percent_change") private double percent_change;
+    @BsonProperty("price") private String price;
+    @BsonProperty("percent_change") private String percent_change;
     @BsonProperty("last_updated") private Date last_updated;
 
 }
