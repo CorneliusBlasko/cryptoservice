326611919593a9704b2f5bd9366c047a3af9b2c5
Alberto <albertomutcirilo@gmail.com>
2020-05-20 09:49:04 +0200

    Modifying tests for the new functionality

--- src/main/java/com/crypto/repositories/Repository.java
+++ src/main/java/com/crypto/repositories/Repository.java
@@ -1,10 +1,12 @@
 package com.crypto.repositories;
 
 import com.crypto.model.CryptoQuote;
-
-import java.util.List;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 
 public interface Repository{
 
-    void saveAll(List<CryptoQuote> quotes,String convert);
+    void saveAll(CryptoQuote quote,String convert);
+
+    MongoCollection<Document> getByCurrency(String convert);
 }
--- src/main/java/com/crypto/utils/Utils.java
+++ src/main/java/com/crypto/utils/Utils.java
@@ -1,40 +1,44 @@
 package com.crypto.utils;
 
+import com.crypto.model.Coin;
 import com.crypto.model.CryptoQuote;
 import com.crypto.model.CryptoResponseData;
 
 import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
+import java.util.*;
 
 public class Utils{
 
-    public static List<String> getCurrencyFromQuote(CryptoResponseData data){
+    /*public static List<String> getCurrencyFromQuote(CryptoResponseData data){
         List<String> currencies = new ArrayList<String>();
         currencies.addAll(data.getQuote().keySet());
         return currencies;
-    }
+    }*/
 
-    public static List<CryptoQuote> CryptoResponseDataToQuote(List<CryptoResponseData> data,String convert){
-        List<CryptoQuote> quotes = new ArrayList<CryptoQuote>();
+    public static CryptoQuote CryptoResponseDataToQuote(List<CryptoResponseData> data,String convert){
+        List<Coin> coins = new ArrayList<>();
 
         NumberFormat numberFormatter = NumberFormat.getNumberInstance(new Locale("es","ES"));
+        CryptoQuote quote = new CryptoQuote();
+        quote.setCurrency(convert);
+        quote.setTimestamp(new Date());
+
 
         for(CryptoResponseData responseData : data){
-            CryptoQuote quote = new CryptoQuote();
-            quote.setName(responseData.getName());
-            quote.setSymbol(responseData.getSymbol());
-            quote.setCurrency(Utils.getCurrencyFromQuote(responseData).get(0));
-            quote.setPrice(numberFormatter.format(responseData.getQuote().get(convert).getPrice()));
-            quote.setPercent_change(numberFormatter.format(responseData.getQuote().get(convert).getPercent_change_24h()));
-            quote.setLast_updated(responseData.getQuote().get(convert).getLast_updated());
+            Coin coin = new Coin();
 
-            quotes.add(quote);
+            coin.setName(responseData.getName());
+            coin.setSymbol(responseData.getSymbol());
+            coin.setPrice(numberFormatter.format(responseData.getQuote().get(convert).getPrice()));
+            coin.setPercent_change(numberFormatter.format(responseData.getQuote().get(convert).getPercent_change_24h()));
+            coin.setLast_updated(responseData.getQuote().get(convert).getLast_updated());
+
+            coins.add(coin);
         }
 
-        return quotes;
+        quote.setData(coins);
+
+        return quote;
     }
 
     public Properties getProperties(){
